2e168a8a84797b223992d3b903919d650f519c93
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-08 09:21:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 //your JS code here. If required.
- document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('DOMContentLoaded', function () {
       // Get the counter and button elements
       var counterElement = document.getElementById('counter');
       var incrementButton = document.getElementById('incrementBtn');
 
-      // Initialize counter
+      // Initialize counter and click count
       var counterValue = 0;
+      var clickCount = 0;
       counterElement.textContent = counterValue;
 
-      // Function to increment the counter and show an alert
+      // Function to increment the counter, show an alert, and limit to three clicks
       function incrementCounter() {
-        counterValue++;
-        counterElement.textContent = counterValue;
-        alert('Counter Value: ' + counterValue);
+        if (clickCount < 3) {
+          counterValue++;
+          counterElement.textContent = counterValue;
+          clickCount++;
+          alert('Counter Value: ' + counterValue);
+        } else {
+          alert('Maximum clicks reached!');
+        }
       }
 
       // Attach the incrementCounter function to the button click event
