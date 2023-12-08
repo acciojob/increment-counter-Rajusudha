17bb6bbeca37ef65ef780b245abc002d715964ea
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-08 09:14:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', function () {
+      // Get the counter and button elements
+      var counterElement = document.getElementById('counter');
+      var incrementButton = document.getElementById('incrementBtn');
+
+      // Initialize counter
+      var counterValue = 0;
+      counterElement.textContent = counterValue;
+
+      // Function to increment the counter and show an alert
+      function incrementCounter() {
+        counterValue++;
+        counterElement.textContent = counterValue;
+        alert('Counter Value: ' + counterValue);
+      }
+
+      // Attach the incrementCounter function to the button click event
+      incrementButton.addEventListener('click', incrementCounter);
+    });
