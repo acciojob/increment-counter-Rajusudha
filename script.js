5bd82eb5bbb7d0e2ca9754d1ae568f1b28f758fb
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-24 21:36:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 const para=document.getElementById("count");
 const btn=document.getElementById("incrementBtn")
-btn.addEventListener("click",function){
+btn.addEventListener("click",function(){
 	let count=Number(para.innerText);
 alert(count);
 para.innerText=++count;
